e8115aeff43274564a477a70ffc6195ed21a7e65
bgpconsultoria <bgpconsultoria@gmail.com>
2016-05-26 11:45:50 -0300

    Create ubnt-up.sh

---
+++ ubnt-up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Limpa e Atualiza Equipamentos UBNT"
+echo ""
+echo "Entre com Login abaixo."
+read login
+echo "Senha."
+read senha
+echo "Bloco de rede, exemplo 192.168.1"
+read rede
+echo "Primeiro ip da rede digitada acima, exemplo 0"
+read ip
+echo "Ultimo ip da rede, exemplo 255"
+read uip
+echo "Porta de acesso SSH, exemplo 22"
+read port
+
+
+uip=`expr $uip + 1`
+while [ "$ip" -lt "$uip" ]; do
+        sshpass -p $senha ssh -p$port -o UserKnownHostsFile=/dev/null -oConnectTimeout=10 -oStrictHostKeyChecking=no $login@$rede.$ip "trigger_url https://raw.githubusercontent.com/bgpconsultoria/scripts-ubnt/script/limpa-atualiza.sh | sh"&
+        ip=`expr $ip + 1`
+done
